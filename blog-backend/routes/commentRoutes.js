0e576745f729061742a169dd8b4505f425892d36
Ishara Gold <isharagold6@gmail.com>
2025-09-23 16:48:34 +0200

    New changes

--- blog-backend/routes/commentRoutes.js
+++ blog-backend/routes/commentRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getComments, createComment, deleteComment } = require("../controllers/commentC");
+const { getComments, getCommentsForPosts,createComment, deleteComment } = require("../controllers/commentC");
 const authMiddleWare = require("../middleware/authM");
 
 
-router.get("/", getComments);
+router.get("/", getComments)
+router.get("/post/:postId", getCommentsForPosts);
 router.post("/", authMiddleWare, createComment);
 router.delete("/:id", deleteComment);
 
